3eb2d541e7eab786f0efc203ced82b5392cdd074
mharley12345 <mharely12345@gmail.com>
2019-06-10 16:06:53 -0700

    n

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from '../components/PostsContainer/PostsContainer';
+import SearchBar from '../components/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <PostsContainer posts={this.state.posts} />
+      </div>
+    );
+  }
 }
 
 export default App;
